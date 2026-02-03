52b8ed40b2439159927676597733fa42ee35f8c1
Alkama <sunasaraalkama0000@gmail.com>
2026-02-03 12:47:55 +0530

    create the common buttons

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,9 +1,18 @@
+"use client"
+
+import Button from "../ui/Button";
 
 export default function HeroSection() {
   return (
     <div>
       <h1>HERO SECTION</h1>
-    </div>
-  )
-}
 
+      <Button
+        variant="primary"
+        onClick={() => alert("Hero button clicked")}
+      >
+        Get Started
+      </Button>
+    </div>
+  );
+}
--- src/components/home/SliderSection.jsx
+++ src/components/home/SliderSection.jsx
@@ -1,8 +1,17 @@
+"use client"
+
+import Button from "../ui/Button";
 
 export default function SliderSection() {
   return (
     <div>
       <h1>SliderSection SECTION</h1>
+       <Button
+              variant="secondry"
+              onClick={() => alert("Hero button clicked")}
+            >
+              Get Started
+            </Button>
     </div>
   )
 }
