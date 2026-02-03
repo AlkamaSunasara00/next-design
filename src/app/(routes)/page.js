99f8dfdfdd49d1f6a34435186da33829d75303bc
asrarmaknojiya <asrarjabir786@gmail.com>
2026-02-03 15:47:28 +0530

    footer completed

--- src/app/(routes)/page.js
+++ src/app/(routes)/page.js
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/home/HeroSection";
 import SliderSection from "@/components/home/SliderSection";
+import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
     <>
-      <HeroSection />
-      <SliderSection />
+      {/* <HeroSection />
+      <SliderSection /> */}
+      <Footer/>
     </>
   );
 }
